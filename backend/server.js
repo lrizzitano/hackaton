a083423540007aff9f1b6e58c06b17731ccf9b4f
Lautaro Agustin Melchiori Callegher <melcho@localhost.localdomain>
2025-03-29 03:29:10 -0300

    agrego conexion a la database

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.listen(5000, () => {
+    connectDB();    // conectamos a la database
     console.log('Server is running on port 5000');
 });
+
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`MongoDB Connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+		process.exit(1); // process code 1 code means exit with failure, 0 means success
+	}
+};
