40b8d0c3ec95068a7a67eec1f04788208e4cc673
Lautaro Agustin Melchiori Callegher <melcho@localhost.localdomain>
2025-03-29 03:13:52 -0300

    agrego configuraciones de enviroment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
